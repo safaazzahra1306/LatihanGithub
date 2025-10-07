6fb1b2f255f0c87d582f67e93f92be154e0d3759
catherine0148 <stefannieputricatherine@gmail.com>
2025-10-07 14:48:43 +0800

    penambahan design pada form siswa

---
+++ Siswa.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace LatihanGithub
+{
+    partial class Siswa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNma = new System.Windows.Forms.Label();
+            this.lblNoTelp = new System.Windows.Forms.Label();
+            this.lblAlmt = new System.Windows.Forms.Label();
+            this.lblKelas = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.rbLaki = new System.Windows.Forms.RadioButton();
+            this.lblJK = new System.Windows.Forms.Label();
+            this.rbPerempuan = new System.Windows.Forms.RadioButton();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNma
+            // 
+            this.lblNma.AutoSize = true;
+            this.lblNma.Location = new System.Drawing.Point(42, 40);
+            this.lblNma.Name = "lblNma";
+            this.lblNma.Size = new System.Drawing.Size(59, 20);
+            this.lblNma.TabIndex = 0;
+            this.lblNma.Text = "Nama :";
+            // 
+            // lblNoTelp
+            // 
+            this.lblNoTelp.AutoSize = true;
+            this.lblNoTelp.Location = new System.Drawing.Point(42, 217);
+            this.lblNoTelp.Name = "lblNoTelp";
+            this.lblNoTelp.Size = new System.Drawing.Size(93, 20);
+            this.lblNoTelp.TabIndex = 1;
+            this.lblNoTelp.Text = "No. Telpon :";
+            // 
+            // lblAlmt
+            // 
+            this.lblAlmt.AutoSize = true;
+            this.lblAlmt.Location = new System.Drawing.Point(42, 157);
+            this.lblAlmt.Name = "lblAlmt";
+            this.lblAlmt.Size = new System.Drawing.Size(67, 20);
+            this.lblAlmt.TabIndex = 2;
+            this.lblAlmt.Text = "Alamat :";
+            // 
+            // lblKelas
+            // 
+            this.lblKelas.AutoSize = true;
+            this.lblKelas.Location = new System.Drawing.Point(42, 96);
+            this.lblKelas.Name = "lblKelas";
+            this.lblKelas.Size = new System.Drawing.Size(56, 20);
+            this.lblKelas.TabIndex = 3;
+            this.lblKelas.Text = "Kelas :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(164, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(207, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "RPL",
+            "AKL",
+            "TKKR",
+            "Perhotelan",
+            "Kimia",
+            "DPB",
+            "ULW"});
+            this.comboBox1.Location = new System.Drawing.Point(164, 96);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(207, 28);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(164, 211);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(207, 26);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(164, 154);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(207, 26);
+            this.textBox3.TabIndex = 7;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(26, 428);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(886, 114);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.Location = new System.Drawing.Point(164, 366);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(101, 38);
+            this.btnSimpan.TabIndex = 9;
+            this.btnSimpan.Text = "Simpan";
+            this.btnSimpan.UseVisualStyleBackColor = true;
+            // 
+            // rbLaki
+            // 
+            this.rbLaki.AutoSize = true;
+            this.rbLaki.Location = new System.Drawing.Point(189, 280);
+            this.rbLaki.Name = "rbLaki";
+            this.rbLaki.Size = new System.Drawing.Size(91, 24);
+            this.rbLaki.TabIndex = 12;
+            this.rbLaki.TabStop = true;
+            this.rbLaki.Text = "Laki-laki";
+            this.rbLaki.UseVisualStyleBackColor = true;
+            // 
+            // lblJK
+            // 
+            this.lblJK.AutoSize = true;
+            this.lblJK.Location = new System.Drawing.Point(42, 279);
+            this.lblJK.Name = "lblJK";
+            this.lblJK.Size = new System.Drawing.Size(114, 20);
+            this.lblJK.TabIndex = 13;
+            this.lblJK.Text = "Jenis Kelamin :";
+            // 
+            // rbPerempuan
+            // 
+            this.rbPerempuan.AutoSize = true;
+            this.rbPerempuan.Location = new System.Drawing.Point(189, 318);
+            this.rbPerempuan.Name = "rbPerempuan";
+            this.rbPerempuan.Size = new System.Drawing.Size(116, 24);
+            this.rbPerempuan.TabIndex = 14;
+            this.rbPerempuan.TabStop = true;
+            this.rbPerempuan.Text = "Perempuan";
+            this.rbPerempuan.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(669, 366);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(101, 38);
+            this.btnEdit.TabIndex = 15;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Location = new System.Drawing.Point(423, 366);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(101, 38);
+            this.btnHapus.TabIndex = 16;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            // 
+            // Siswa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(939, 579);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.rbPerempuan);
+            this.Controls.Add(this.lblJK);
+            this.Controls.Add(this.rbLaki);
+            this.Controls.Add(this.btnSimpan);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblKelas);
+            this.Controls.Add(this.lblAlmt);
+            this.Controls.Add(this.lblNoTelp);
+            this.Controls.Add(this.lblNma);
+            this.Name = "Siswa";
+            this.Text = "Siswa";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNma;
+        private System.Windows.Forms.Label lblNoTelp;
+        private System.Windows.Forms.Label lblAlmt;
+        private System.Windows.Forms.Label lblKelas;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btnSimpan;
+        private System.Windows.Forms.RadioButton rbLaki;
+        private System.Windows.Forms.Label lblJK;
+        private System.Windows.Forms.RadioButton rbPerempuan;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnHapus;
+    }
+}
